9b90273726a5fc95e7207e2c7d4ab9a48a2a6927
scurtis7 <scurtis7@gmail.com>
2019-12-13 20:48:36 -0500

    add table display of teams

--- src/webapp/src/Teams.js
+++ src/webapp/src/Teams.js
@@ -1,32 +1,69 @@
 import React, {Component} from "react";
+import './Teams.css';
 import axios from 'axios';
-import JSONPretty from 'react-json-pretty';
-import 'react-json-pretty/themes/monikai.css';
+import {Table} from 'reactstrap';
 
 class Teams extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            teams: null,
+            teams: [],
         };
         this.setTeams = this.setTeams.bind(this);
+        this.loadTableData = this.loadTableData.bind(this);
     }
 
     setTeams(teams) {
-        this.setState({ teams });
+        this.setState({teams});
+    }
+
+    loadTableData() {
+        if (this.state.teams === null) {
+            return (
+                <tr><td colSpan="6">Loading...</td></tr>
+            );
+        } else {
+            return this.state.teams.map((team, index) => {
+                const {id, code, fullName, shortName, conference, division} = team; //destructuring
+                return (
+                    <tr key={index}>
+                        <td>{id}</td>
+                        <td>{code}</td>
+                        <td>{fullName}</td>
+                        <td>{shortName}</td>
+                        <td>{conference}</td>
+                        <td>{division}</td>
+                    </tr>
+                );
+            });
+        }
     }
 
     componentDidMount() {
         axios(`http://localhost:8080/api/teams`)
-            .then(teams => this.setTeams(teams))
+            .then(teams => this.setTeams(teams.data))
             .catch(error => error);
     }
 
     render() {
         return (
-            <div>
-                <JSONPretty data={this.state.teams} theme={1337} onJSONPrettyError={e => console.error(e)} />
+            <div className="Teams-div">
+                <Table className="Teams-table">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Code</th>
+                        <th>Full Name</th>
+                        <th>Short Name</th>
+                        <th>Conference</th>
+                        <th>Division</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.loadTableData()}
+                    </tbody>
+                </Table>
             </div>
         );
     }
